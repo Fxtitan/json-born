4f20698132b58025b096e684fdac357ab814c2a6
Sharod Bailey <Sharod.Bailey@codeimmersives.com>
2020-04-27 15:22:05 -0400

    not done

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 const fs = require('fs')
+
+
+
+fs.readFile('./users.json', function(error, data) {
+  if (error) {
+    throw error;
+  }
+
+  const actualObj = JSON.parse(data);
+  console.log(actualObj)
+})
+
+
+//asynchroness fs.readFile
+
+ GET users[1]
